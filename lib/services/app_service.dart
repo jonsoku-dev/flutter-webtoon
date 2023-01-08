1365e9f78ddc6d9b87af6cc3ee7220a1272d2a12
jongseok-lee <jongseok.lee@linecorp.com>
2023-01-08 21:56:53 +0900

    feat(webtoon): fromJson

--- lib/services/app_service.dart
+++ lib/services/app_service.dart
@@ -4,15 +4,15 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon.dart';
 
 class ApiService {
-  List<WebtoonModel> webtoonInstances = [];
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
   Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      final List<dynamic> webtoons = jsonDecode(response.body);
+      final webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
         final toon = WebtoonModel.fromJson(webtoon);
         webtoonInstances.add(toon);
