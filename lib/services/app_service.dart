9efa219bae95b0c9abbcafe3d61906bfc23dac1c
jongseok-lee <jongseok.lee@linecorp.com>
2023-01-10 23:15:53 +0900

    feat(webtoon): ApiService

--- lib/services/app_service.dart
+++ lib/services/app_service.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon.dart';
+import 'package:toonflix/models/webtoon_detail.dart';
+import 'package:toonflix/models/webtoon_episode.dart';
 
 class ApiService {
   static const String baseUrl =
@@ -19,6 +21,34 @@ class ApiService {
         webtoonInstances.add(toon);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      final toon = WebtoonDetailModel.fromJson(webtoon);
+      return toon;
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+    String id,
+  ) async {
+    List<WebtoonEpisodeModel> webtoonEpisodeInstances = [];
+    final url = Uri.parse('$baseUrl/$id/latest');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoonEpisodes = jsonDecode(response.body);
+      for (var webtoonEpisode in webtoonEpisodes) {
+        final toon = WebtoonEpisodeModel.fromJson(webtoonEpisode);
+        webtoonEpisodeInstances.add(toon);
+      }
+      return webtoonEpisodeInstances;
     }
     throw Error();
   }
